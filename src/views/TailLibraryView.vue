365c4a89fce04ee57e1f6843365c12eefbc71f12
Demoad <adriprev95@gmail.com>
2023-08-03 02:37:31 +0200

    fix: the fecth load

--- src/views/TailLibraryView.vue
+++ src/views/TailLibraryView.vue
@@ -1,21 +1,29 @@
 <template>
-    <section class="h-screen bg-no-repeat bg-center bg-cover bg-[url('/src/assets/img/6313772.jpg')] saturate-50">
-        <main class="flex flex-col h-screen w-screen bg-gradient-to-b from-black to-transparent">
-            <header
-                class="pt-24 mx-5 sm:mx-11 md:mx-20 lg:mx-40 2xl:mx-72 flex flex-col gap-6 sm:gap-8 md:gap-10 lg:gap-12 xl:gap-20">
-                <h1
-                    class="text-center justify-center font-abhayasemibold font-extrabold text-4xl sm:text-7xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl text-white">
-                    Historias de otro mundo
-                </h1>
+    <Suspense>
+        <template #default>
+            <section class="h-screen bg-no-repeat bg-center bg-cover bg-[url('/src/assets/img/6313772.jpg')] saturate-50">
+                <main class="flex flex-col h-screen w-screen bg-gradient-to-b from-black to-transparent">
+                    <header
+                        class="pt-24 mx-5 sm:mx-11 md:mx-20 lg:mx-40 2xl:mx-72 flex flex-col gap-6 sm:gap-8 md:gap-10 lg:gap-12 xl:gap-20">
+                        <h1
+                            class="text-center justify-center font-abhayasemibold font-extrabold text-4xl sm:text-7xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl text-white">
+                            Historias de otro mundo
+                        </h1>
 
-            </header>
-            <section
-                class="grow grid grid-cols-1 px-20 gap-9 justify-center content-center place-content-start place-items-center">
-                <TailCardComponent v-for="tail in listTails" :key="tail.id" :id="tail.id" :img-url="tail.poster_path">
-                </TailCardComponent>
+                    </header>
+                    <section
+                        class="grow grid grid-cols-1 px-20 gap-9 justify-center content-center place-content-start place-items-center">
+                        <TailCardComponent v-for="tail in listTails" :key="tail.id" :id="tail.id"
+                            :img-url="tail.poster_path">
+                        </TailCardComponent>
+                    </section>
+                </main>
             </section>
-        </main>
-    </section>
+        </template>
+        <template #fallback>
+            <h1 class=" text-4xl text-center">Loading...</h1>
+        </template>
+    </Suspense>
 </template>
 
 <script lang="ts" setup>
--- src/views/TailView.vue
+++ src/views/TailView.vue
@@ -1,25 +1,32 @@
 <template>
-    <div>
-        <section
-            class="h-screen bg-no-repeat bg-center bg-cover bg-[url('/src/assets/img/6313772.jpg')] saturate-50 z-10 fixed overflow-hidden">
-            <main class="flex flex-col h-screen w-screen bg-gradient-to-b from-black to-transparent">
-                <button class="fixed w-5 md:w-10 mt-5 ml-5 rotate-90 animate-pulse"
-                    @click="$router.push({ name: 'tailslibrary' })">
-                    <ArrowDown></ArrowDown>
-                </button>
-                <button class="fixed  bottom-0 right-0 w-5 md:w-10 mb-5 mr-5 animate-pulse" @click="scrollY">
-                    <ArrowDown></ArrowDown>
-                </button>
-                <section class="scrollbar-hide scroll-smooth overflow-scroll snap-y snap-mandatory" id="scroll">
-                    <div v-for="page in pages" :key="page.page_num"
-                        class="grid grid-cols-1 sm:grid-cols-2 justify-center content-center h-screen snap-always snap-center overscroll-none">
-                        <PageComponent :photo_text="page.photo_text" :text="page.text" :id="page.page_num"></PageComponent>
-                    </div>
+    <Suspense>
+        <template #default>
+            <div>
+                <section
+                    class="h-screen bg-no-repeat bg-center bg-cover bg-[url('/src/assets/img/6313772.jpg')] saturate-50 z-10 fixed overflow-hidden">
+                    <main class="flex flex-col h-screen w-screen bg-gradient-to-b from-black to-transparent">
+                        <button class="fixed w-5 md:w-10 mt-5 ml-5 rotate-90 animate-pulse"
+                            @click="$router.push({ name: 'tailslibrary' })">
+                            <ArrowDown></ArrowDown>
+                        </button>
+                        <button class="fixed  bottom-0 right-0 w-5 md:w-10 mb-5 mr-5 animate-pulse" @click="scrollY">
+                            <ArrowDown></ArrowDown>
+                        </button>
+                        <section class="scrollbar-hide scroll-smooth overflow-scroll snap-y snap-mandatory" id="scroll">
+                            <div v-for="page in pages" :key="page.page_num"
+                                class="grid grid-cols-1 sm:grid-cols-2 justify-center content-center h-screen snap-always snap-center overscroll-none">
+                                <PageComponent :photo_text="page.photo_text" :text="page.text" :id="page.page_num">
+                                </PageComponent>
+                            </div>
+                        </section>
+                    </main>
                 </section>
-            </main>
-        </section>
-
-    </div>
+            </div>
+        </template>
+        <template #fallback>
+            Loading
+        </template>
+    </Suspense>
 </template>
 
 <script setup lang="ts">
@@ -28,8 +35,11 @@ import { getTailById } from '@/utils/ApiAcces'
 import { onBeforeMount } from 'vue'
 import type { Page, Tail } from '@/utils/typeTail';
 import { useRoute } from 'vue-router';
+
 import PageComponent from '@/components/PageComponent.vue';
 import ArrowDown from '@/assets/img/ArrowDown.vue';
+
+
 const route = useRoute();
 const id = route.params.id;
 const tail = ref<Tail>()
